203e253c205e6b8d4b58125b3a7e429df0d39abe
Jason Choe <jasonkmchoe@gmail.com>
2017-05-05 17:30:35 -0400

    working on models

--- models/index.js
+++ models/index.js
@@ -1,5 +1,48 @@
 const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost:5432/trip-planner');
 
-module.exports = db;
+const Place = db.define('place', {
+  address: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  city: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  state: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  phone: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  location: {
+    type: Sequelize.ARRAY(DataTypes.DECIMAL),
+    allowNull: false
+  }
+}
 
+
+  state: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  state: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  state: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  state: {
+    type: Sequelize.STRING,
+    allowNull, false
+  },
+});
+
+
+
+module.exports = db;
